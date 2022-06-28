e24d8cc93392fd89a8c7caf2a9fe138e58186e24
Chun, Emily <emily.chun@intel.com>
2022-06-28 16:59:40 +0900

    add SC model templates

--- external/model-preparation-algorithm/configs/segmentation/ocr-lite-hrnet-18-cls-incr/model.py
+++ external/model-preparation-algorithm/configs/segmentation/ocr-lite-hrnet-18-cls-incr/model.py
@@ -1,3 +1,9 @@
 _base_ = [
-    '../../../submodule/samples/cfgs/models/backbones/ote_litehrnet18.yaml',
+  '../../../submodule/samples/cfgs/models/backbones/ote_litehrnet18.yaml',
+  '../../../submodule/recipes/stages/_base_/models/segmentors/seg_class_incr.py'
 ]
+# _base_ = [
+#   '../../../submodule/models/segmentation/ocr_litehrnet18.yaml',
+# ]
+
+load_from = 'https://storage.openvinotoolkit.org/repositories/openvino_training_extensions/models/custom_semantic_segmentation/litehrnet18_imagenet1k_rsc.pth'
---
+++ external/model-preparation-algorithm/configs/segmentation/ocr-lite-hrnet-s-mod2-cls-incr/model.py
@@ -0,0 +1,7 @@
+# _base_ = [
+#  '../../../submodule/samples/cfgs/models/backbones/ote_litehrnet_s_mod2.yaml',
+#  '../../../submodule/recipes/stages/_base_/models/segmentors/seg_class_incr_small.py'
+# ]
+_base_ = [
+  '../../../submodule/models/segmentation/ocr_litehrnet_s_mod2.yaml',
+]
