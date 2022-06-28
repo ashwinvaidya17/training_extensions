9915385742efe4c31694deaddc7311ae79788c04
Chun, Emily <emily.chun@intel.com>
2022-06-29 00:01:18 +0900

    update config

--- external/model-preparation-algorithm/configs/segmentation/ocr-lite-hrnet-s-mod2-cls-incr/model.py
+++ external/model-preparation-algorithm/configs/segmentation/ocr-lite-hrnet-s-mod2-cls-incr/model.py
@@ -5,3 +5,5 @@
 _base_ = [
   '../../../submodule/models/segmentation/ocr_litehrnet_s_mod2.yaml',
 ]
+
+load_from = 'https://storage.openvinotoolkit.org/repositories/openvino_training_extensions/models/custom_semantic_segmentation/litehrnet18_imagenet1k_rsc.pth'
