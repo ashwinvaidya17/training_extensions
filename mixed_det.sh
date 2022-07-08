532904bde111e0b6819966c3a4a8e47083caed2b
Lee, Soobee <soobee.lee@intel.com>
2022-07-08 22:00:23 +0900

    train time

--- mixed_det.sh
+++ mixed_det.sh
@@ -22,7 +22,7 @@ do
     --val-ann-files ../ote_poc_data/det/coco-annotation/$data/instances_val2017.1234@10.0_OLD_NEW.json \
     --val-data-roots ../ote_poc_data/det/coco/images/val2017 \
     --load-weights ../ote_poc_data/det/pretrained/mr50/initial_model/weights.pth \
-    --save-model-to $output_path/${model}/${data}/$mode params --learning_parameters.num_iters 1"
+    --save-model-to $output_path/${model}/${data}/$mode"
     echo $cmd; CUDA_VISIBLE_DEVICES=0 $cmd &> $output_path/${model}/${data}/$mode/train.log
 
     cmd="ote eval \
@@ -30,7 +30,7 @@ do
     --test-ann-files ../ote_poc_data/det/coco-annotation/$data/instances_test2017.1234@100.0_OLD_NEW.json \
     --test-data-roots ../ote_poc_data/det/coco/images/val2017 \
     --load-weights  $output_path/${model}/${data}/$mode/weights.pth"
-    echo $cmd; CUDA_VISIBLE_DEVICES=0 $cmd &> $output_path/${model}/${data}/test.log
+    echo $cmd; CUDA_VISIBLE_DEVICES=0 $cmd &> $output_path/${model}/${data}/$mode/test.log
 
     #lvis
     data="LVIS-subsets"
@@ -43,8 +43,8 @@ do
     --val-ann-files ../ote_poc_data/det/coco-annotation/$data/lvis_val_seed1234@10.0_OLD_NEW_1CLS.json \
     --val-data-roots ../ote_poc_data/det/coco/images \
     --load-weights ../ote_poc_data/det/pretrained/initial_model_hpo/weights.pth \
-    --save-model-to $output_path/${model}/${data}/$mode params --learning_parameters.num_iters 1"
-    echo $cmd; CUDA_VISIBLE_DEVICES=0 $cmd &> $output_path/${model}/${data}/$mode/test.log
+    --save-model-to $output_path/${model}/${data}/$mode"
+    echo $cmd; CUDA_VISIBLE_DEVICES=0 $cmd &> $output_path/${model}/${data}/$mode/train.log
 
     cmd="ote eval \
     external/model-preparation-algorithm/configs/detection/$model/template.yaml \
@@ -56,6 +56,7 @@ done
 
 
 git checkout sc-intg-ins-seg
+git submodule update --init --recursive
 mode="fp32"
 
 for model in "${model_name[@]}"
@@ -73,7 +74,7 @@ do
     --val-ann-files ../ote_poc_data/det/coco-annotation/$data/instances_val2017.1234@10.0_OLD_NEW.json \
     --val-data-roots ../ote_poc_data/det/coco/images/val2017 \
     --load-weights ../ote_poc_data/det/pretrained/mr50/initial_model/weights.pth \
-    --save-model-to $output_path/${model}/${data}/$mode params --learning_parameters.num_iters 1"
+    --save-model-to $output_path/${model}/${data}/$mode"
     echo $cmd; CUDA_VISIBLE_DEVICES=0 $cmd &> $output_path/${model}/${data}/$mode/train.log
 
     cmd="ote eval \
@@ -81,7 +82,7 @@ do
     --test-ann-files ../ote_poc_data/det/coco-annotation/$data/instances_test2017.1234@100.0_OLD_NEW.json \
     --test-data-roots ../ote_poc_data/det/coco/images/val2017 \
     --load-weights  $output_path/${model}/${data}/$mode/weights.pth"
-    echo $cmd; CUDA_VISIBLE_DEVICES=0 $cmd &> $output_path/${model}/${data}/test.log
+    echo $cmd; CUDA_VISIBLE_DEVICES=0 $cmd &> $output_path/${model}/${data}/$mode/test.log
 
     #lvis
     data="LVIS-subsets"
@@ -94,8 +95,8 @@ do
     --val-ann-files ../ote_poc_data/det/coco-annotation/$data/lvis_val_seed1234@10.0_OLD_NEW_1CLS.json \
     --val-data-roots ../ote_poc_data/det/coco/images \
     --load-weights ../ote_poc_data/det/pretrained/initial_model_hpo/weights.pth \
-    --save-model-to $output_path/${model}/${data}/$mode params --learning_parameters.num_iters 1"
-    echo $cmd; CUDA_VISIBLE_DEVICES=0 $cmd &> $output_path/${model}/${data}/$mode/test.log
+    --save-model-to $output_path/${model}/${data}/$mode"
+    echo $cmd; CUDA_VISIBLE_DEVICES=0 $cmd &> $output_path/${model}/${data}/$mode/train.log
 
     cmd="ote eval \
     external/model-preparation-algorithm/configs/detection/$model/template.yaml \
