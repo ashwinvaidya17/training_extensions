eaf792237938fdd6ab6226888d13c24f14639143
Lee, Soobee <soobee.lee@intel.com>
2022-07-11 15:40:24 +0900

    fp16 for seg

--- external/model-preparation-algorithm/configs/segmentation/ocr-lite-hrnet-x-mod3/model.py
+++ external/model-preparation-algorithm/configs/segmentation/ocr-lite-hrnet-x-mod3/model.py
@@ -3,3 +3,7 @@ _base_ = [
 ]
 
 load_from = 'https://storage.openvinotoolkit.org/repositories/openvino_training_extensions/models/custom_semantic_segmentation/litehrnetxv3_imagenet1k_rsc.pth'
+# fp16 settings
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
+# fp16 placeholder
+fp16 = dict()
