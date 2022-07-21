9807334e321a8132e09e93ce2ba1e1a52d416b6e
harimkan <harim.kang@intel.com>
2022-07-21 14:11:53 +0900

    Some code added for MAE score support

--- external/mmdetection/configs/custom-counting-instance-seg/efficientnetb2b_maskrcnn/model.py
+++ external/mmdetection/configs/custom-counting-instance-seg/efficientnetb2b_maskrcnn/model.py
@@ -125,7 +125,8 @@ model = dict(
             mask_thr_binary=0.5)))
 
 cudnn_benchmark = True
-evaluation = dict(interval=1, metric='mAP', save_best='mAP', iou_thr=[0.5 , 0.55, 0.6 , 0.65, 0.7 , 0.75, 0.8 , 0.85, 0.9, 0.95])
+# evaluation = dict(interval=1, metric=['mAP', 'mae'], save_best='mAP')
+evaluation = dict(interval=1, metric=['mAP', 'mae'], save_best='mAP')
 optimizer = dict(type='SGD', lr=0.015, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 lr_config = dict(
