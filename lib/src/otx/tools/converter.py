c5f9fe4cc6544b06803d381a0bab7338e63e20e0
Ashwin Vaidya <ashwin.vaidya@intel.com>
2025-09-24 14:42:31 +0200

    Rename templates
    Signed-off-by: Ashwin Vaidya <ashwin.vaidya@intel.com>

--- lib/src/otx/tools/converter.py
+++ lib/src/otx/tools/converter.py
@@ -154,7 +154,7 @@ TEMPLATE_ID_MAPPING = {
     "YOLO_Ultralytics_v8n": {
         "recipe_path": RECIPE_PATH / "detection" / "yolo_ultralytics_v8n.yaml",
         "status": ModelStatus.ACTIVE,
-        "default": True,
+        "default": False,
     },
     "YOLO_Ultralytics_v11n": {
         "recipe_path": RECIPE_PATH / "detection" / "yolo_ultralytics_v11n.yaml",
